98397be8b2f0973d7082220dd1bc7f9361e6278e
Ludis-ET <leulsegedmelaku1020@gmail.com>
2023-11-25 11:03:54 +0300

    deploy

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -78,13 +78,22 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'gee332fAFfd2d6ABe334F6EC6EdfcE6b',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '19508',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -139,3 +148,4 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#PU5cSkKqm1FbSmmOdm9gsQ
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,4 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin-ludis/', admin.site.urls),
     path('',include('core.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
